1baf792b38ebe0338315dc31445f461c34d9141c
Kaiser <kaiserzulkarnain@gmail.com>
2024-01-21 13:45:01 +0700

    fixing ui bug

--- components/Calculator.tsx
+++ components/Calculator.tsx
@@ -102,12 +102,15 @@ const Calculator = () => {
   return (
     <form
       onSubmit={calculateAge}
-      className="w-auto h-auto sm:w-[375px] p-12 sm:p-5 pr-8 sm:pr-0 bg-white rounded-3xl rounded-br-[180px] sm:rounded-br-[100px] sm:flex sm:flex-col sm:items-center "
+      className=" flex flex-col w-[750px] h-auto sm:w-[365px] p-12 sm:p-8 pr-8 bg-white rounded-3xl rounded-br-[180px] sm:rounded-br-[100px] sm:flex sm:flex-col sm:items-center sm:justify-center "
     >
       {/* input box */}
       <div className="flex gap-x-7 sm:gap-x-5">
-        <div className="flex flex-col gap-y-2 ">
-          <label id="dayLabel" className="text-lg font-bold text-[#716f6f]">
+        <div className="flex flex-col gap-y-2 sm:gap-y-1 ">
+          <label
+            id="dayLabel"
+            className="text-lg sm:text-[17px] font-bold text-[#716f6f]"
+          >
             DAY
           </label>
           <input
@@ -116,14 +119,19 @@ const Calculator = () => {
             id="dayInput"
             type="number"
             placeholder="DD"
-            className="w-36 sm:w-24 p-4 text-3xl sm:text-2xl placeholder-[#6f6d6d] opacity-95 font-extrabold border-2 rounded-lg"
+            className="w-36 sm:w-24 p-4 text-3xl sm:text-xl placeholder-[#6f6d6d] opacity-95 font-extrabold border-2 rounded-lg focus:outline-[#141414]"
           />
           {erroMessage && (
-            <div className="text-red-500 text-[13px] italic">{erroMessage}</div>
+            <div className="text-red-500 sm:text-[9px] italic">
+              {erroMessage}
+            </div>
           )}
         </div>
-        <div className="flex flex-col gap-y-2">
-          <label id="monthLabel" className="text-lg font-bold text-[#716f6f]">
+        <div className="flex flex-col gap-y-2 sm:gap-y-1">
+          <label
+            id="monthLabel"
+            className="text-lg sm:text-[17px] font-bold text-[#716f6f]"
+          >
             MONTH
           </label>
           <input
@@ -132,14 +140,19 @@ const Calculator = () => {
             id="monthInput"
             type="number"
             placeholder="MM"
-            className="w-36 sm:w-24 p-4 text-3xl sm:text-2xl placeholder-[#6f6d6d] opacity-95 font-extrabold border-2 rounded-lg"
+            className="w-36 sm:w-24 p-4 text-3xl sm:text-xl placeholder-[#6f6d6d] opacity-95 font-extrabold border-2 rounded-lg focus:outline-[#141414]"
           />
           {erroMessage && (
-            <div className="text-red-500 text-[13px] italic">{erroMessage}</div>
+            <div className="text-red-500 text-[13px] sm:text-[9px] italic">
+              {erroMessage}
+            </div>
           )}
         </div>
-        <div className="flex flex-col gap-y-2">
-          <label id="yearLabel" className="text-lg font-bold  text-[#716f6f]">
+        <div className="flex flex-col gap-y-2 sm:gap-y-1">
+          <label
+            id="yearLabel"
+            className="text-lg sm:text-[17px] font-bold  text-[#716f6f]"
+          >
             YEAR
           </label>
           <input
@@ -148,23 +161,27 @@ const Calculator = () => {
             id="yearInput"
             type="number"
             placeholder="YYYY"
-            className="w-36 sm:w-24 p-4 text-3xl sm:text-2xl placeholder-[#6f6d6d] opacity-95 font-extrabold border-2 rounded-lg"
+            className="w-36 sm:w-24 p-4 text-3xl sm:text-xl placeholder-[#6f6d6d] opacity-95 font-extrabold border-2 rounded-lg focus:outline-[#141414]"
           />
           {erroMessage && (
-            <div className="text-red-500 text-[13px] italic">{erroMessage}</div>
+            <div className="text-red-500 text-[13px] sm:text-[9px] italic">
+              {erroMessage}
+            </div>
           )}
         </div>
       </div>
       {/* button */}
-      <div className="w-[650px] mt-2 flex items-center">
-        <hr className="w-[85%] sm:w-[43%]" />
-        <button
-          type="submit"
-          className="w-20 h-20 sm:w-16 sm:h-16 sm:my-5 bg-[#854cff] hover:bg-[#141414] flex items-center justify-center rounded-full"
-        >
-          <ArrowIcon />
-        </button>
-        <hr className="w-[85%] sm:w-[43%] lg:hidden md:hidden" />
+      <div className="w-full mt-2 flex items-center">
+        <div className="border-2 w-full "></div>
+        <div>
+          <button
+            type="submit"
+            className="w-20 h-20 sm:w-16 sm:h-16 sm:my-5 bg-[#854cff] hover:bg-[#141414] flex items-center justify-center rounded-full"
+          >
+            <ArrowIcon />
+          </button>
+        </div>
+        <div className="border-2 w-full lg:hidden md:hidden"></div>
       </div>
       {/* Input result */}
       <div className="flex flex-col gap-y-2">
